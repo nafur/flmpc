2b46b42092a9ae22e08abd3adf4c04bca14f10f1
Gereon Kremer <gereon.kremer@rwth-aachen.de>
2013-02-23 16:34:00 +0100

    some more stuff

--- static/shared.js
+++ static/shared.js
@@ -51,3 +51,72 @@ $.fn.dataTableExt.oApi.fnReloadAjax = function ( oSettings, sNewSource, fnCallba
         }
     }, oSettings );
 };
+
+/*
+ *	toggle css class for some id
+ */
+function toggleClass(id, data, cls) {
+	if (data == 1) $(id).addClass(cls);
+	else $(id).removeClass(cls);
+}
+
+/*
+ *	build track string from track object
+ */
+function trackName(track) {
+	if (track["Name"]) return track["Name"];
+	if (track["Artist"] && track["Title"]) {
+		if (track["Album"]) return track["Artist"] + " - " + track["Album"] + " - " + track["Title"];
+		return track["Artist"] + " - " + track["Title"];
+	}
+	return track["file"];
+}
+function buildTrack(data, type, full) {
+	return trackName(full);
+}
+
+/*
+ *	format time below a day
+ */
+function formatTime(sec) {
+	if (!sec) return "--";
+	if (typeof sec == String) {
+		if (sec.indexOf(":") > -1) sec = sec.substr(0, sec.indexOf(":"));
+	}
+	
+	var sec = sec % (60*60*24) >> 0;
+
+	var secs = sec % 60;
+	if (secs < 10) secs = "0" + secs;
+	sec = (sec - secs) / 60 >> 0;
+
+	var mins = sec % 60;
+	if (mins < 10) mins = "0" + mins;
+	sec = (sec - mins) / 60 >> 0;
+	
+	if (sec > 0) return sec + ":" + mins + ":" + secs;
+	else return mins + ":" + secs;
+}
+
+/*
+ *	format time with weeks and days
+ */
+function formatLongTime(sec) {
+	if (!sec) return "--";
+	if (typeof sec == String) {
+		if (sec.indexOf(":") > -1) sec = sec.substr(0, sec.indexOf(":"));
+	}
+	var res = "";
+		
+	if (sec > 60*60*24*7) {
+		res += (sec / (60*60*24*7) >> 0) + "w ";
+		sec %= 60*60*24*7;
+	}
+	if (sec > 60*60*24) {
+		res += (sec / (60*60*24) >> 0) + "d ";
+		sec %= 60*60*24;
+	}
+	res += formatTime(sec);
+
+	return res;
+}
